cbe2895c0cd05b2a38469cfb091a5babbe06cc25
Igor Brito <igorbritoandrade@gmail.com>
2025-08-28 15:43:48 -0300

    shorten lichaotree, remove BumpAllocator and friends

--- content/data-structures/lichaoTree.h
+++ content/data-structures/lichaoTree.h
@@ -9,34 +9,26 @@ ll eval(pair<int, int> f, int x) { return f.first * 1LL * x + f.second; }
 struct no {
 	int bg, nd, md;
 	no *l, *r;
-	pair<int, int> best;
+	pii best;
 	no(int bgg, int ndd) : bg(bgg), nd(ndd), md((bg + nd) >> 1), best(-1e11, -1e18){
-		if(bg != nd)
-			l = new no(bg, md), r = new no(md + 1, nd);
+		if(bg != nd) l = new no(bg, md), r = new no(md + 1, nd);
 	}
-	void upd(int a, int b, pair<int, int> val) {
-		if(b < bg || nd < a)
-			return;
+	void upd(int a, int b, pii val) {
+		if(b < bg || nd < a) return;
 		if(a <= bg && nd <= b)
 		{
 			bool lef = eval(val, bg) > eval(best, bg);
 			bool mid = eval(val, md) > eval(best, md);
-			if(mid)
-				swap(val, best);
-			if(bg == nd)
-				return;
-			if(lef != mid)
-				l->upd(a, b, val);
-			else
-				r->upd(a, b, val);
+			if(mid) swap(val, best);
+			if(bg == nd) return;
+			if(lef != mid) l->upd(a, b, val);
+			else r->upd(a, b, val);
 			return;
 		}
-		l->upd(a, b, val);
-		r->upd(a, b, val);
+		l->upd(a, b, val); r->upd(a, b, val);
 	}
 	ll get(int pos) {
-		if(bg == nd)
-			return eval(best, pos);
+		if(bg == nd) return eval(best, pos);
 		ll ret = eval(best, pos);
 		return max(ret, (pos <= md ? l : r)->get(pos));
 	}
