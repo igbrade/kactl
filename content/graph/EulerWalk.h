4f94f5f9d2e09750b39f5a522deadec77b91c078
Igor Brito <igorbritoandrade@gmail.com>
2025-08-28 11:46:47 -0300

    add recursive euler walk description

--- content/graph/EulerWalk.h
+++ content/graph/EulerWalk.h
@@ -29,3 +29,11 @@ vi eulerWalk(vector<vector<pii>>& gr, int nedges, int src=0) {
 	for (int x : D) if (x < 0 || sz(ret) != nedges+1) return {};
 	return {ret.rbegin(), ret.rend()};
 }
+/*
+ * Recursive euler walk
+ * procedure FindEulerPath(V)
+ * 1. iterate through all the edges outgoing from vertex V;
+ *	remove this edge from the graph,
+ *	and call FindEulerPath from the second end of this edge;
+ * 2. add vertex V to the answer.
+*/
