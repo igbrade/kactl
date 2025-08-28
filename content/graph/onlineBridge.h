9eb2b7aef7061eda8cef28781b196c8cd02d928d
Pedro Mesquita <p183998@dac.unicamp.br>
2025-08-28 15:51:10 -0300

    Shorten onlineBridge.h

--- content/graph/onlineBridge.h
+++ content/graph/onlineBridge.h
@@ -1,35 +1,28 @@
 /**
  * Author: unknown, copied from cp-algorithms https://cp-algorithms.com/graph/bridge-searching-online.html
  * Description: Find bridges online, after each edge addition
-*/
-vector<int> par, dsu_2ecc, dsu_cc, dsu_cc_size;
-int bridges;
-int lca_iteration;
-vector<int> last_visit;
-
+ * Time: $O(n \log n + m \log n)$
+ */
+vi par, _2ecc, _cc, _cc_sz, last_visit;
+int bridges, lca_it;
 void init(int n) {
     par.resize(n);
-    dsu_2ecc.resize(n);
-    dsu_cc.resize(n);
-    dsu_cc_size.resize(n);
-    lca_iteration = 0;
+    _2ecc.resize(n); _cc.resize(n); _cc_sz.resize(n);
+    lca_it = bridges = 0;
     last_visit.assign(n, 0);
-    for (int i=0; i<n; ++i) {
-        dsu_2ecc[i] = i;
-        dsu_cc[i] = i;
-        dsu_cc_size[i] = 1;
+    rep(i, 0, n) {
+        _2ecc[i] = _cc[i] = i;
+        _cc_sz[i] = 1;
         par[i] = -1;
     }
-    bridges = 0;
 }
 int find_2ecc(int v) {
-    if (v == -1)
-        return -1;
-    return dsu_2ecc[v] == v ? v : dsu_2ecc[v] = find_2ecc(dsu_2ecc[v]);
+    if (v == -1) return -1;
+    return _2ecc[v] == v ? v : _2ecc[v] = find_2ecc(_2ecc[v]);
 }
 int find_cc(int v) {
     v = find_2ecc(v);
-    return dsu_cc[v] == v ? v : dsu_cc[v] = find_cc(dsu_cc[v]);
+    return _cc[v] == v ? v : _cc[v] = find_cc(_cc[v]);
 }
 void make_root(int v) {
     int root = v;
@@ -37,69 +30,54 @@ void make_root(int v) {
     while (v != -1) {
         int p = find_2ecc(par[v]);
         par[v] = child;
-        dsu_cc[v] = root;
+        _cc[v] = root;
         child = v;
         v = p;
     }
-    dsu_cc_size[root] = dsu_cc_size[child];
+    _cc_sz[root] = _cc_sz[child];
 }
 void merge_path (int a, int b) {
-    ++lca_iteration;
-    vector<int> path_a, path_b;
+    ++lca_it;
+    vi path_a, path_b;
     int lca = -1;
     while (lca == -1) {
         if (a != -1) {
             a = find_2ecc(a);
             path_a.push_back(a);
-            if (last_visit[a] == lca_iteration){
-                lca = a;
-                break;
-                }
-            last_visit[a] = lca_iteration;
+            if (last_visit[a] == lca_it) { lca = a; break; }
+            last_visit[a] = lca_it;
             a = par[a];
         }
         if (b != -1) {
             b = find_2ecc(b);
             path_b.push_back(b);
-            if (last_visit[b] == lca_iteration){
-                lca = b;
-                break;
-                }
-            last_visit[b] = lca_iteration;
+            if (last_visit[b] == lca_it) { lca = b; break; }
+            last_visit[b] = lca_it;
             b = par[b];
         }
-
     }
     for (int v : path_a) {
-        dsu_2ecc[v] = lca;
-        if (v == lca)
-            break;
+        _2ecc[v] = lca;
+        if (v == lca) break;
         --bridges;
     }
     for (int v : path_b) {
-        dsu_2ecc[v] = lca;
-        if (v == lca)
-            break;
+        _2ecc[v] = lca;
+        if (v == lca) break;
         --bridges;
     }
 }
-
 void add_edge(int a, int b) {
     a = find_2ecc(a); b = find_2ecc(b);
-    if (a == b)
-        return;
-
+    if (a == b) return;
     int ca = find_cc(a);
     int cb = find_cc(b);
-
     if (ca != cb) {
         ++bridges;
-        if (dsu_cc_size[ca] > dsu_cc_size[cb]) {
-            swap(a, b); swap(ca, cb);
-        }
+        if (_cc_sz[ca] > _cc_sz[cb]) swap(a, b), swap(ca, cb);
         make_root(a);
-        par[a] = dsu_cc[a] = b;
-        dsu_cc_size[cb] += dsu_cc_size[a];
+        par[a] = _cc[a] = b;
+        _cc_sz[cb] += _cc_sz[a];
         return;
     }
     merge_path(a, b);
